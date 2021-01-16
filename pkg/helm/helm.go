0ea50d47500f4de2e50d0a41953ce11e45fd9c88
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-01-15 17:42:13 -0800

    Implement merging helm index

--- pkg/helm/helm.go
+++ pkg/helm/helm.go
@@ -14,11 +14,33 @@ import (
 func CreateOrUpdateHelmIndex(rootFs billy.Filesystem) error {
 	absRepositoryAssetsDir := filesystem.GetAbsPath(rootFs, path.RepositoryAssetsDir)
 	absRepositoryHelmIndexFile := filesystem.GetAbsPath(rootFs, path.RepositoryHelmIndexFile)
-	helmIndexFile, err := helmRepo.IndexDirectory(absRepositoryAssetsDir, path.RepositoryAssetsDir)
+
+	var helmIndexFile *helmRepo.IndexFile
+
+	// Load index file from disk if it exists
+	exists, err := filesystem.PathExists(rootFs, path.RepositoryHelmIndexFile)
+	if err != nil {
+		return fmt.Errorf("Encountered error while checking if Helm index file already exists in repository: %s", err)
+	} else if exists {
+		helmIndexFile, err = helmRepo.LoadIndexFile(absRepositoryHelmIndexFile)
+		if err != nil {
+			return fmt.Errorf("Encountered error while trying to load existing index file: %s", err)
+		}
+	} else {
+		helmIndexFile = helmRepo.NewIndexFile()
+	}
+
+	// Generate the current index file from the assets/ directory
+	newHelmIndexFile, err := helmRepo.IndexDirectory(absRepositoryAssetsDir, path.RepositoryAssetsDir)
 	if err != nil {
 		return fmt.Errorf("Encountered error while trying to generate new Helm index: %s", err)
 	}
+
+	// Merge the indices and sort them
+	helmIndexFile.Merge(newHelmIndexFile)
 	helmIndexFile.SortEntries()
+
+	// Write new index to disk
 	err = helmIndexFile.WriteFile(absRepositoryHelmIndexFile, os.ModePerm)
 	if err != nil {
 		return fmt.Errorf("Encountered error while trying to write updated Helm index into index.yaml: %s", err)

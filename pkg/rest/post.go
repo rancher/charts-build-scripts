b2b5f0e81ff2568706edbe81c08bfe8c0aa68989
Lucas Lopes <lucas.lopes@suse.com>
2023-08-11 13:02:44 -0300

    Adding token to Docker Hub requests if credentials are set

---
+++ pkg/rest/post.go
@@ -0,0 +1,52 @@
+package rest
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// Post sends a POST request to the given URL with the given body and decodes the response into the given response model.
+func Post(url string, body, responseModel any) error {
+
+	// Marshal the body
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return fmt.Errorf("error marshalling the body: %v", err)
+	}
+
+	// Create a new POST request
+	req, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return fmt.Errorf("error creating the POST request: %v", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	// Create a new HTTP client
+	client := &http.Client{
+		Timeout: time.Second * 2,
+	}
+
+	// Send the request
+	response, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("error sending the POST request: %v", err)
+	}
+	defer response.Body.Close()
+
+	// Check the status code
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("received unexpected status code %d with message: %v", response.StatusCode, response.Body)
+	}
+
+	// Decode the response body
+	err = json.NewDecoder(response.Body).Decode(responseModel)
+	if err != nil {
+		return fmt.Errorf("error decoding the response body: %v", err)
+	}
+
+	return nil
+}

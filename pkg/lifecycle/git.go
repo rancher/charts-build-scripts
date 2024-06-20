aef0aec0a3c5d481673b889022d415cee94f9a36
nicholasSSUSE <nicholas.paolillo@suse.com>
2024-06-20 17:06:18 -0300

    adding function listProdAndDevAssets
    Several steps here:
    1. create a temporary directory structure to clone the charts repository.
    2. destroy it after script execution cleaning everything.
    3. clone the charts repository with --depth=1 to speed up the process.
    
    This function will execute the comparisons between prod and dev branches, populating the following already declared maps:
    	assetsReleasedInLifecycle       map[string][]Asset // OK if not empty
    	assetsNotReleasedOutLifecycle   map[string][]Asset // OK if not empty
    	assetsNotReleasedInLifecycle    map[string][]Asset // WARN if not empty
    	assetsReleasedOutLifecycle      map[string][]Asset // ERROR if not empty

--- pkg/lifecycle/git.go
+++ pkg/lifecycle/git.go
@@ -4,8 +4,29 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+
+	"github.com/sirupsen/logrus"
 )
 
+type Git struct {
+	Dir string
+}
+
+// cloneAtDir clones a repository at a given directory
+func cloneAtDir(url, dir string) (*Git, error) {
+	logrus.Infof("Cloning repository %s into %s", url, dir)
+	cmd := exec.Command("git", "clone", "--depth", "1", url, dir)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		logrus.Errorf("error while cloning repository: %s; err: %v", url, err)
+		return nil, fmt.Errorf("error while cloning repository: %s", err)
+	}
+	return &Git{Dir: dir}, nil
+}
+
+// checkIfGitIsClean checks if the git repository is clean and,
+// returns true if it is clean, false otherwise
 func checkIfGitIsClean(debug bool) (bool, error) {
 	cmd := exec.Command("git", "status", "--porcelain")
 	if debug {
@@ -24,8 +45,9 @@ func checkIfGitIsClean(debug bool) (bool, error) {
 	return true, nil
 }
 
+// gitAddAndCommit stages all changes and commits them with a given message,
+// equivalent to: git add -A && git commit -m message
 func gitAddAndCommit(message string) error {
-
 	// Stage all changes, including deletions
 	cmd := exec.Command("git", "add", "-A")
 	if err := cmd.Run(); err != nil {
--- pkg/lifecycle/status.go
+++ pkg/lifecycle/status.go
@@ -1,6 +1,10 @@
 package lifecycle
 
-import "github.com/sirupsen/logrus"
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
 
 // Status struct hold the results of the assets versions comparison,
 // this data will all be logged and saves into log files for further analysis
@@ -23,9 +27,16 @@ type Status struct {
 //	separate the assets to be released from the assets to be forward ported
 func (ld *Dependencies) getStatus() (*Status, error) {
 	status := &Status{ld: ld}
+
 	// List the current assets versions in the current branch
 	status.listCurrentAssetsVersionsOnTheCurrentBranch()
+
 	// List the production and development assets versions comparisons from the default branches
+	err := status.listProdAndDevAssets()
+	if err != nil {
+		logrus.Errorf("Error while comparing production and development branches: %s", err)
+		return status, err
+	}
 
 	// Separate the assets to be released from the assets to be forward ported after the comparison
 
@@ -80,4 +91,73 @@ func (s *Status) listCurrentAssetsVersionsOnTheCurrentBranch() {
 	s.assetsOutLifecycleCurrentBranch = outsideLifecycle
 
 	return
+}
+
+// listProdAndDevAssets will clone the charts repository at a temporary directory,
+// fetch and checkout in the production and development branches for the given version,
+// get the assets versions from the index.yaml file and compare the assets versions,
+// separating into 4 different maps for further analysis.
+func (s *Status) listProdAndDevAssets() error {
+	// Create and destroy a temporary directory structure
+	defaultWorkingDir, tempDir, err := createTemporaryDirStructure()
+	if err != nil {
+		logrus.Errorf("Error while creating temporary dir structure: %s", err)
+		return err
+	}
+	defer destroyTemporaryDirStructure(defaultWorkingDir, tempDir)
+
+	// Clone the repository at the temporary directory
+	git, err := cloneAtDir("https://github.com/rancher/charts", tempDir)
+	if err != nil {
+		return err
+	}
+
+	_ = git // this will be removed in the future
+
+	// Fetch, checkout and map assets versions in the production and development branches
+
+	// Compare the assets versions between the production and development branches
+	return nil
+}
+
+// createTemporaryDirStructure creates a temporary directory structure and changes the working directory to it returning the path for both folders.
+func createTemporaryDirStructure() (string, string, error) {
+	// Save the current working directory for changing back to it later
+	defaultWorkingDir, err := os.Getwd()
+	if err != nil {
+		logrus.Errorf("Error while getting the current working directory: %s", err)
+		return "", "", err
+	}
+
+	// Create the temporary directory
+	tempDir, err := os.MkdirTemp("", "temporaryDir")
+	if err != nil {
+		return "", "", err
+	}
+
+	// change the workind directory to the temporary one
+	err = os.Chdir(tempDir)
+	if err != nil {
+		logrus.Errorf("Error while changing working directory to temporary directory: %v", err)
+		return defaultWorkingDir, tempDir, err
+	}
+	return defaultWorkingDir, tempDir, nil
+}
+
+// destroyTemporaryDirStructure destroys the temporary directory and changes the working directory back to the default one.
+func destroyTemporaryDirStructure(defaultWorkingDir, tempDir string) error {
+	// Change the directory back to the default working directory
+	err := os.Chdir(defaultWorkingDir)
+	if err != nil {
+		logrus.Errorf("Error while changing back to default working directory: %v", err)
+		return err
+	}
+
+	// Remove the temporary directory
+	err = os.RemoveAll(tempDir)
+	if err != nil {
+		logrus.Errorf("Error while removing temporary directory: %v", err)
+		return err
+	}
+	return nil
 }

33d4416c9611429d11aa26e3e803a673f78b23a1
nicholasSUSE <nicholas.paolillo@suse.com>
2025-10-02 03:15:36 -0300

    improving safety while pushing helm charts to oci registry

--- pkg/auto/oci.go
+++ pkg/auto/oci.go
@@ -119,9 +119,24 @@ func setupHelm(ociDNS, ociUser, ociPass string, debug bool) (*registry.Client, e
 	return regClient, nil
 }
 
+// update will attempt to update a helm chart to an OCI registry.
+// 2 phases:
+//   - 1: Pre-Flight validations (check the current chart + check if it already exists)
+//   - 2: Push
 func (o *oci) update(ctx context.Context, release *options.ReleaseOptions) ([]string, error) {
 	var pushedAssets []string
 
+	// List of assets to process
+	type assetInfo struct {
+		chart   string
+		version string
+		asset   string
+		data    []byte
+	}
+	var assetsToProcess []assetInfo
+
+	// Phase 1: Pre-Flight Validations
+	logger.Log(ctx, slog.LevelDebug, "Phase 1: Pre-Flight Validations")
 	for chart, versions := range *release {
 		for _, version := range versions {
 			asset := chart + "-" + version + ".tgz"
@@ -139,17 +154,51 @@ func (o *oci) update(ctx context.Context, release *options.ReleaseOptions) ([]st
 				return pushedAssets, err
 			}
 			if exists {
-				logger.Log(ctx, slog.LevelError, "asset already exists", slog.String("asset", asset))
-				return pushedAssets, errors.New("asset already exists in the OCI registry: " + asset)
+				// Skip existing charts instead of failing
+				logger.Log(ctx, slog.LevelWarn, "chart already exists in registry, will skip",
+					slog.String("asset", asset))
+				continue
 			}
 
-			logger.Log(ctx, slog.LevelDebug, "pushing asset to OCI Registry", slog.String("asset", asset))
-			if err := o.push(o.helmClient, assetData, buildPushURL(o.DNS, chart, version)); err != nil {
-				logger.Log(ctx, slog.LevelError, "failed to push asset", slog.String("asset", asset))
-				return pushedAssets, err
-			}
-			pushedAssets = append(pushedAssets, asset)
+			logger.Log(ctx, slog.LevelDebug, "asset valid and doesn't exist in the registry already", slog.String("asset", asset))
+			assetsToProcess = append(assetsToProcess, assetInfo{
+				chart:   chart,
+				version: version,
+				asset:   asset,
+				data:    assetData,
+			})
 		}
+	}
+
+	// check if there is anything to push
+	if len(assetsToProcess) == 0 {
+		logger.Log(ctx, slog.LevelInfo, "no new charts to push - all charts already exist in registry")
+		return pushedAssets, nil
+	}
+
+	// Phase 2
+	var pushErrors []error
+	logger.Log(ctx, slog.LevelInfo, "Phase 2: Push")
+	for _, info := range assetsToProcess {
+		logger.Log(ctx, slog.LevelDebug, "pushing", slog.String("asset", info.asset))
+
+		if err := o.push(o.helmClient, info.data, buildPushURL(o.DNS, info.chart, info.version)); err != nil {
+			logger.Log(ctx, slog.LevelError, "failed to push asset", slog.String("asset", info.asset))
+			pushErrors = append(pushErrors, errors.New("asset: "+info.asset+" error: "+err.Error()))
+			continue
+		}
+		pushedAssets = append(pushedAssets, info.asset)
+		logger.Log(ctx, slog.LevelInfo, "pushed", slog.String("asset", info.asset))
+	}
+
+	if len(pushErrors) > 0 {
+		logger.Log(ctx, slog.LevelError, "push phase completed with errors",
+			slog.Int("successful", len(pushedAssets)),
+			slog.Int("failed", len(pushErrors)))
+		for _, err := range pushErrors {
+			logger.Err(err)
+		}
+		return pushedAssets, errors.New("some assets failed, please fix and retry only these assets")
 	}
 
 	return pushedAssets, nil

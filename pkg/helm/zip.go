96fd1de3956263fda7291a0e63659461566130cf
nicholasSUSE <nicholas.paolillo@suse.com>
2025-12-18 14:46:43 -0300

    re-organize zip pkg inside helm pkg

--- pkg/helm/zip.go
+++ pkg/helm/zip.go
@@ -1,4 +1,4 @@
-package zip
+package helm
 
 import (
 	"context"
@@ -8,10 +8,9 @@ import (
 	"strings"
 
 	"github.com/go-git/go-billy/v5"
+	"github.com/rancher/charts-build-scripts/pkg/config"
 	"github.com/rancher/charts-build-scripts/pkg/filesystem"
-	"github.com/rancher/charts-build-scripts/pkg/helm"
 	"github.com/rancher/charts-build-scripts/pkg/logger"
-	"github.com/rancher/charts-build-scripts/pkg/path"
 
 	helmLoader "helm.sh/helm/v3/pkg/chart/loader"
 )
@@ -19,15 +18,19 @@ import (
 // ArchiveCharts zips charts from charts/ into assets/. If the asset was re-ordered, it will also update charts/.
 // If specificChart is provided, it will filter the set of charts that will be targeted for zipping.
 // It will also not update an asset if its internal contents have not changed.
-func ArchiveCharts(ctx context.Context, repoRoot string, specificChart string) error {
-	repoFs := filesystem.GetFilesystem(repoRoot)
+func ArchiveCharts(ctx context.Context, specificChart string) error {
+	cfg, err := config.FromContext(ctx)
+	if err != nil {
+		return err
+	}
+
 	foundChart := false
 	zipChart := func(ctx context.Context, fs billy.Filesystem, helmChartPath string, isDir bool) error {
 		if !isDir || len(strings.Split(helmChartPath, "/")) != 3 {
 			// We expect to be at charts/{chart}/{version}
 			return nil
 		}
-		chartVersionPath, err := filesystem.MovePath(ctx, helmChartPath, path.RepositoryChartsDir, "")
+		chartVersionPath, err := filesystem.MovePath(ctx, helmChartPath, config.PathChartsDir, "")
 		if err != nil {
 			return fmt.Errorf("unable to get chart version path for %s", helmChartPath)
 		}
@@ -38,8 +41,8 @@ func ArchiveCharts(ctx context.Context, repoRoot string, specificChart string) e
 			}
 		}
 		foundChart = true
-		chartAssetsDirpath := filepath.Join(path.RepositoryAssetsDir, filepath.Dir(chartVersionPath))
-		tgzPath, err := helm.GenerateArchive(ctx, repoFs, fs, helmChartPath, chartAssetsDirpath, nil)
+		chartAssetsDirpath := filepath.Join(config.PathAssetsDir, filepath.Dir(chartVersionPath))
+		tgzPath, err := GenerateArchive(ctx, cfg.RootFS, fs, helmChartPath, chartAssetsDirpath, nil)
 		if err != nil {
 			return fmt.Errorf("encountered error while trying to update archive based on chart in %s: %s", chartVersionPath, err)
 		}
@@ -47,21 +50,21 @@ func ArchiveCharts(ctx context.Context, repoRoot string, specificChart string) e
 		// that is created reorders the contents of Chart.yaml / requirements.yaml to be
 		// alphabetical. Therefore, when zipping a chart we always need to unzip the finalized
 		// chart(s) back to the charts/ directory, which is done by calling UnzipAsset after this.
-		currentAsset, err := filesystem.MovePath(ctx, tgzPath, path.RepositoryAssetsDir, "")
+		currentAsset, err := filesystem.MovePath(ctx, tgzPath, config.PathAssetsDir, "")
 		if err != nil {
 			return err
 		}
-		if err := DumpAssets(ctx, repoRoot, currentAsset); err != nil {
+		if err := DumpAssets(ctx, currentAsset); err != nil {
 			return fmt.Errorf("encountered error while trying to update chart %s based on %s: %s", chartVersionPath, tgzPath, err)
 		}
 		return nil
 	}
 
-	if err := filesystem.WalkDir(ctx, repoFs, path.RepositoryChartsDir, zipChart); err != nil {
+	if err := filesystem.WalkDir(ctx, cfg.RootFS, config.PathChartsDir, config.IsSoftError(ctx), zipChart); err != nil {
 		return fmt.Errorf("encountered error while trying to zip charts: %s", err)
 	}
 	if len(specificChart) > 0 && !foundChart {
-		return fmt.Errorf("could not find chart at %s/%s in repository", path.RepositoryChartsDir, specificChart)
+		return fmt.Errorf("could not find chart at %s/%s in repository", config.PathChartsDir, specificChart)
 	}
 
 	return nil
@@ -69,15 +72,19 @@ func ArchiveCharts(ctx context.Context, repoRoot string, specificChart string) e
 
 // DumpAssets unzips assets from assets/ into charts/.
 // If specificAsset is provided, it will filter the set of assets that will be targeted for unzipping.
-func DumpAssets(ctx context.Context, repoRoot string, specificAsset string) error {
-	repoFs := filesystem.GetFilesystem(repoRoot)
+func DumpAssets(ctx context.Context, specificAsset string) error {
+	cfg, err := config.FromContext(ctx)
+	if err != nil {
+		return err
+	}
+
 	foundAsset := false
 	unzipAsset := func(ctx context.Context, fs billy.Filesystem, tgzPath string, isDir bool) error {
 		if isDir || len(strings.Split(tgzPath, "/")) != 3 || filepath.Ext(tgzPath) != ".tgz" {
 			// We expect to be at assets/{chart}/{chart}-{version}.tgz
 			return nil
 		}
-		assetPath, err := filesystem.MovePath(ctx, tgzPath, path.RepositoryAssetsDir, "")
+		assetPath, err := filesystem.MovePath(ctx, tgzPath, config.PathAssetsDir, "")
 		if err != nil {
 			return fmt.Errorf("unable to get tgz path for %s", tgzPath)
 		}
@@ -99,7 +106,7 @@ func DumpAssets(ctx context.Context, repoRoot string, specificAsset string) erro
 			return fmt.Errorf("could not load Helm chart: %s", err)
 		}
 		// Unarchive tgz
-		chartChartsDirpath := filepath.Join(path.RepositoryChartsDir, chart.Metadata.Name, chart.Metadata.Version)
+		chartChartsDirpath := filepath.Join(config.PathChartsDir, chart.Metadata.Name, chart.Metadata.Version)
 		// If we remove an overlay file, the file will not be removed from the charts directory if it already exists,
 		// the easiest way to solve this problem is to clean the target directory before un-archiving the chart's package
 		if err := filesystem.RemoveAll(fs, chartChartsDirpath); err != nil {
@@ -114,12 +121,12 @@ func DumpAssets(ctx context.Context, repoRoot string, specificAsset string) erro
 		return nil
 	}
 
-	if err := filesystem.WalkDir(ctx, repoFs, path.RepositoryAssetsDir, unzipAsset); err != nil {
+	if err := filesystem.WalkDir(ctx, cfg.RootFS, config.PathAssetsDir, config.IsSoftError(ctx), unzipAsset); err != nil {
 		return fmt.Errorf("encountered error while trying to zip charts: %s", err)
 	}
 
 	if len(specificAsset) > 0 && !foundAsset {
-		return fmt.Errorf("could not find asset at %s/%s in repository", path.RepositoryAssetsDir, specificAsset)
+		return fmt.Errorf("could not find asset at %s/%s in repository", config.PathAssetsDir, specificAsset)
 	}
 	return nil
 }
